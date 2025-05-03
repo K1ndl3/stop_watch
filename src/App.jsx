169f7c6b575c550112459a7e1aa3150047bf7ca7
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-03 13:58:21 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="stop-watch">
+        <div className="display"></div>
+        <div className="start-button"></div>
+        <div className="stop-button"></div>
+    </div>
+    </>
+  );
+}
+
+export default App
