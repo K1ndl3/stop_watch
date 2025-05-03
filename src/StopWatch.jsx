b4e068971446d5db54feb60ba5550e981156d50d
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-03 16:09:30 -0700

    finshed tut

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -7,33 +7,69 @@ function StopWatch() {
     const startTimeRef = useRef(0);
 
     useEffect(() =>{
+
+        if (isRunning) {
+            intervalRef.current = setInterval(() =>{
+                setElapsedTime(Date.now() - startTimeRef.current)
+            }, 10)
+        }
+
+        return () => {
+            clearInterval(intervalRef.current);
+        }
          
     }, [isRunning]);
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
 
     function stop(){
-
+        setIsRunning(false);
     }
 
     function reset(){
-
+        setElapsedTime(0);
+        setIsRunning(false);
     }
 
-    function formatTime(){
-
-        return '00:00:00';
+    function formatTime() {
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+        let seconds = Math.floor((elapsedTime / 1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+    
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`;
     }
  
     return (
         <div className="stop-watch">
             <div className="display">{formatTime()}</div>
             <div className="controls">
-                <button onClick={start()} className="start-button">Start</button>
-                <button onClick={stop()} className="stop-button">Stop</button>
-                <button onClick={reset()} className="reset-button">Reset</button>
+                <button onClick={start} className="start-button">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                </svg>
+                </button>
+
+                <button onClick={stop} className="stop-button">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                </svg>
+
+                </button>
+                <button onClick={reset} className="reset-button">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+
+                </button>
             </div>
         </div>
     );
