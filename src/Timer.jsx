259be2338053e444455589118dced37125c37008
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-04 19:12:12 -0700

    added UI for timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -15,12 +15,24 @@ function Timer() {
 
     return (
     <>
-    <div className="Timer">
-        <div className="Display">{DisplayTime()}</div>
-        <div className="Controls">
-            <button className="start-timer">start-timer</button>
-            <button className="stop-timer">stop-timer</button>
-            <button className="reset-timer">reset-timer</button>
+    <div className="timer">
+        <div className="timer-display">{DisplayTime()}</div>
+        <div className="timer-controls">
+            <button className="start-timer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                </svg>
+            </button>
+            <button className="stop-timer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                </svg>
+            </button>
+            <button className="reset-timer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                </svg>
+            </button>
         </div>
     </div>
     </>);
