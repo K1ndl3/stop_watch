837afc42315e3a295c4f8b5680b5fadbb1c78a3c
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-04 18:38:44 -0700

    added toggle mode functionality

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -6,14 +6,22 @@ function Timer() {
     }
 
     function StopTimer(){
-        
+
+    }
+
+    function DisplayTime(){
+        return '00:00:00'
     }
 
     return (
     <>
     <div className="Timer">
-        <div className="Display"></div>
-        <div className="Controls"></div>
+        <div className="Display">{DisplayTime()}</div>
+        <div className="Controls">
+            <button className="start-timer">start-timer</button>
+            <button className="stop-timer">stop-timer</button>
+            <button className="reset-timer">reset-timer</button>
+        </div>
     </div>
     </>);
 
