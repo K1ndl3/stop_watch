4810b02e473121519bda6c7716bbcb6328d1a2c2
K1ndl3 <HUYBUI02@csu.fullerton.edu>
2025-05-04 18:18:33 -0700

    added documentation for timer feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import StopWatch from "./StopWatch";
-
+import Timer from "./Timer";
+import React, {useState} from "react";
 function App() {
+  const [isStopWatch, setIsStopWatch] = useState(true);
 
+  function SwitchMode(){
+    setIsStopWatch(!isStopWatch);
+  }
 
   return (
     <>
-      <StopWatch></StopWatch>
+      {(isStopWatch) ? <StopWatch></StopWatch> : <Timer></Timer>}
+      <button className="switch-mode" onClick={SwitchMode}>Switch Mode</button>
     </>
   );
 }
---
+++ src/Timer.jsx
@@ -0,0 +1,22 @@
+
+function Timer() {
+
+    function StartTimer(){
+
+    }
+
+    function StopTimer(){
+        
+    }
+
+    return (
+    <>
+    <div className="Timer">
+        <div className="Display"></div>
+        <div className="Controls"></div>
+    </div>
+    </>);
+
+}
+
+export default Timer;
